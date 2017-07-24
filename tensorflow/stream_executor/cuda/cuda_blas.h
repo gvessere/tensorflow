9492b542df43ea49b160af15793be02e2f3fb376
Ben Barsdell <bbarsdell@nvidia.com>
2017-07-24 16:41:27 -0700

    Add support for CUBLAS_TENSOR_OP_MATH in fp16 GEMM
    - Applies to matrix multiplications with fp16 input/output.
      Computations will fall back to pseudo-fp16 if tensor op math is
      disabled or not supported.
    - Enabled by default, but can be disabled by setting the environment
      variable TF_DISABLE_TENSOR_OP_MATH=1.

--- tensorflow/stream_executor/cuda/cuda_blas.h
+++ tensorflow/stream_executor/cuda/cuda_blas.h
@@ -84,21 +84,23 @@ class CUDABlas : public blas::BlasSupport {
   template <typename FuncT, typename... Args>
   bool DoBlasInternalImpl(FuncT cublas_func, Stream *stream,
                           bool pointer_mode_host, bool err_on_failure,
-                          Args... args);
+                          bool use_tensor_op_math, Args... args);
 
   // Convenience functions that call DoBlasInternalImpl with different values
   // for err_on_failure.
   template <typename FuncT, typename... Args>
   bool DoBlasInternal(FuncT cublas_func, Stream *stream, bool pointer_mode_host,
-                      Args... args) {
+                      bool use_tensor_ops, Args... args) {
     return DoBlasInternalImpl(cublas_func, stream, pointer_mode_host,
-                              /*err_on_failure=*/true, args...);
+                              /*err_on_failure=*/true, use_tensor_ops, args...);
   }
   template <typename FuncT, typename... Args>
   bool DoBlasInternalFailureOK(FuncT cublas_func, Stream *stream,
-                               bool pointer_mode_host, Args... args) {
+                               bool pointer_mode_host, bool use_tensor_ops,
+                               Args... args) {
     return DoBlasInternalImpl(cublas_func, stream, pointer_mode_host,
-                              /*err_on_failure=*/false, args...);
+                              /*err_on_failure=*/false, use_tensor_ops,
+                              args...);
   }
 
   // A helper function to implement DoBlasGemmBatched interfaces for generic
